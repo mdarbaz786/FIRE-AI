1dac09c9cd73e43e1a5e5a7692fa1e8a60ad3ad1
mdarbaz786 <152139727+mdarbaz786@users.noreply.github.com>
2024-12-03 18:37:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const ownervb = process.env.OWNERS || "2349013432338; Opaksfire"
+const ownervb = process.env.OWNERS || "917478172267; Opaksfire"
 
 
 const ownerlist = ownervb.split(';');
